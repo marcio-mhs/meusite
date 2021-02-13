5a30b390ff0e4a94b1dff25f0a5cffa6dfb98b8e
marcio <marciomhs@hotmail.com>
2021-02-13 08:59:33 -0300

    aula 35

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import React, {Component} from 'react';
 
-class Equipe extends Component {
-  render () {
-    return (
-      <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade}/>
-    );
-  }
-}
+class App extends Component{
 
-class Sobre extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: 'Marcio',
+      contador: 0
+    };
+
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+  aumentar(){
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state);
+  }
+
+  diminuir(){
+    let state = this.state;
+    state.contador -= 1;
+    this.setState(state);
+  }
+
   render(){
     return (
       <div>
-        <h2>Olá {this.props.nome}</h2>
-        <h3>Cargo {this.props.cargo}</h3>
-        <h3>idade {this.props.idade}</h3>
-        <hr/>
+        <h1>Contador:</h1>  
+        {this.state.nome}      
+        <h3>          
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h3>
+        
       </div>
-    );    
+    );
   }
-}
-
-function App() {
-  return (
-    <div>
-        <h1>Conheça nossa equipe</h1> 
-        <Equipe nome="Marcio" cargo="Programador" idade="38" />
-    </div>
-  );
 }
 
 export default App;
