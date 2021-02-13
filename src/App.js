745eaaa1600bf34ab10589a8ada4d024869a76b8
marcio <marciomhs@hotmail.com>
2021-02-12 23:48:03 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const Bemvindo = (props) => {
+    return(
+        <div>
+            <h2>Bem vindo {props.nome}</h2>
+            <h3>Tenho {props.idade} anos.</h3>
+        </div>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        Olá Mundo!
+        <Bemvindo nome="Marcio" idade="38"/>
+        <Bemvindo nome="Cremilda" idade="49"/>
+        <h1>Curso React</h1> 
     </div>
   );
 }
