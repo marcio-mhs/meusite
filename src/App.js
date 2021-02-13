b9c5c1bfc356e40fb0b168ab0405997c914b49ef
marcio <marciomhs@hotmail.com>
2021-02-13 09:49:26 -0300

    aula 37

--- src/App.js
+++ src/App.js
@@ -5,36 +5,24 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      nome: 'Marcio',
-      contador: 0
+      hora: '00:00:00'
     };
-
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
-  }
-  aumentar(){
-    let state = this.state;
-    state.contador += 1;
-    this.setState(state);
   }
 
-  diminuir(){
-    let state = this.state;
-    state.contador -= 1;
-    this.setState(state);
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({hora: new Date().toLocaleTimeString()})
+    }, 1000);
   }
 
+  componentDidUpdate(){
+    console.log("Atualizou");
+  }
+  
   render(){
     return (
       <div>
-        <h1>Contador:</h1>  
-        {this.state.nome}      
-        <h3>          
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
-        
+        <h1>Meu projeto {this.state.hora}</h1>         
       </div>
     );
   }
