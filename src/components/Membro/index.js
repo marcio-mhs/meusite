740dac2ecef9bf7558752630f096921bed9ab4c9
marcio <marciomhs@hotmail.com>
2021-02-13 10:16:01 -0300

    aula 40

--- src/components/Membro/index.js
+++ src/components/Membro/index.js
@@ -4,23 +4,34 @@ class Membro extends Component{
     constructor(props){
         super(props);
         this.state = {
-            nome: props.nome
-        }
+            status: false
+        };
+        this.sair = this.sair.bind(this);
         this.entrar = this.entrar.bind(this);
     }
 
-    entrar(nome){
-        this.setState({nome: nome})
+    entrar(){
+        this.setState({status: true});
     }
 
-    
-  
+    sair(){
+        this.setState({status: false});
+    }
+
   render(){
     return (
       <div>
-        <h2>Bem vindo(a) {this.state.nome}</h2>
-        <button onClick={() => this.entrar('Marcio')}>Entrar no sistema</button>
-        <button onClick={() => this.setState({nome: ''})}>Sair</button>
+        {this.state.status ?
+            <div>
+                <h2>Bem vindo ao Sistema</h2>
+                <button onClick={this.sair}>Sair</button>
+            </div> :
+            <div>
+                Faça o login
+                <button onClick={this.entrar}>Entrar no Sistema</button>
+            </div>
+        }
+        
       </div>
     );
   }
