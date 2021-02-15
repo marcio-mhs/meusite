4f984b68e4a5b8967a261aedca595abdca3872df
marcio-mhs <marciomhs@hotmail.com>
2021-02-15 15:32:50 -0300

    Aula 48 - Projeto Todo-List

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import TodoList from './components/TodoList';
 import './style.css';
 //https://sujeitoprogramador.com/rn-api/?api=post
 
@@ -7,37 +8,15 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      nutri:[],
+      
     };
-  }
-
-  componentDidMount(){
-    let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
-    fetch(url)
-    .then((r) => r.json())
-    .then((json) => {
-      let state = this.state;
-      state.nutri = json;
-      this.setState(state);
-    });
   }
 
   render(){
     return (
       <div className="container">
-        <header>
-          <strong>Nutri</strong>
-        </header>
-        {this.state.nutri.map((item)=>{
-            return(
-              <article key={item.id} className="post">
-                <strong className="titulo">{item.titulo}</strong>
-                <img className="capa" src={item.capa}/>
-                <p className="subtitulo">{item.subtitulo}</p>
-                <a className="botao" href="#">Acessar</a>
-              </article>
-            );
-        })}
+        <h1>Lista de tarefas</h1>
+        <TodoList/>
       </div>      
     );
   }
